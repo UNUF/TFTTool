459344f2bceaf3b0dd6de68ad9aca43953afd714
MMMZZZZ <36891873+MMMZZZZ@users.noreply.github.com>
2022-01-19 22:59:00 +0100

    Formatting

--- NextionChecksum.py
+++ NextionChecksum.py
@@ -2,6 +2,7 @@
 Nextion uses a CRC32 type algorithm.
 """
 
+
 class Checksum:
     def CRC(self, data, salt: int = 0xffffffff, skip: int = 0, length: int = -1):
         if length < 0:
@@ -13,7 +14,7 @@ class Checksum:
 
     def _CRC32(self, l: list, xorIn=0xffffffff, xorOut=0):
         poly = 0x4c11db7
-        poly |= (1 << 32) # kill the bit that would normally be shifted out of the register
+        poly |= (1 << 32)  # kill the bit that would normally be shifted out of the register
         reg = 0
         l = l + [0]
         l[0] ^= xorIn
@@ -26,6 +27,6 @@ class Checksum:
                 word &= 0xffffffff
                 if reg >= (1 << 32):
                     reg ^= poly
-        reg &= 0xffffffff#((1 << 32) - 1)
+        reg &= 0xffffffff  # ((1 << 32) - 1)
         reg ^= xorOut
         return reg
